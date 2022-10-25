e2fd860b6cee02c1bbe6f5cedca58ff721bf09df
ogola5 <ogolaevance5@gmail.com>
2022-10-25 16:53:33 +0300

    completed assignment

---
+++ lib/song.rb
@@ -0,0 +1,51 @@
+class Song
+    attr_accessor :name,:artist,:genre
+    @@count=0
+    @@artist=[]
+    @@genre=[]
+
+    def initialize(name,artist,genre)
+        @name=name
+        @artist=artist
+        @genre=genre
+        @@count+=1
+        @@genres<<genre
+        @@artist<<artist
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artist.uniq
+    end
+    def self.artist_count
+        artist_count{}
+        @@artist.each do|artist|
+            if artist_count[artist]
+                artist_count[artist]+=1
+            else
+                artist_count[artist]=1
+            end
+        end
+        artist_count
+    end
+    def self.genres
+        @@genres.uniq
+    end
+    def self.genre_count
+        genre_count{}
+        @@genres.each do|genre|
+            if genre_count[genre]
+                genre_count[genre]+=1
+
+                
+            else
+                genre_count[genre]=1
+                
+            end
+        end
+        genre_count
+    end
+end
